ed2eae5ca761e654f847e819cee9f40e6c88df01
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-06-03 12:36:51 +0100

    modif front

--- public/build/assets/chevron-left-DZi0_Qj0.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-ChgF6VS7.js";var t=e(`ChevronLeftIcon`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
--- public/build/assets/chevron-right-_28QhvVa.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-ChgF6VS7.js";var t=e(`ChevronRightIcon`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
