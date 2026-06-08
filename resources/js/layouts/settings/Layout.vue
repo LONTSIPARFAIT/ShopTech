5a7f236a15a3f827d687ce36f835e6ce5b259e24
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-06-08 13:49:10 +0100

    modif front

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -56,7 +56,7 @@ const { isCurrentOrParentUrl } = useCurrentUrl();
                 : 'text-muted-foreground hover:bg-secondary hover:text-foreground'
         ]"
     >
-        <component :is="item.icon" class="h-4 w-4" />p
+        <component :is="item.icon" class="h-4 w-4" />
         {{ item.title }}
     </Link>
 </nav>
